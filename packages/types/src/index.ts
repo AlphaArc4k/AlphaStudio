4f6374b31d261925a08de471b704396115aca457
dyslaxic <3xdevvv@gmail.com>
2025-01-22 13:15:06 +0000

    add types package

---
+++ packages/types/src/index.ts
@@ -0,0 +1,53 @@
+
+export interface AgentConfig {
+  id ?: string;
+  configVersion: string;
+  isDeployed?: boolean;
+  info: {
+    name: string;
+    description: string;
+    version?: string;
+    isPublic?: boolean;
+    profileImage?: string;
+    character: string;
+    task: string;
+  };
+  data: {
+    enabledViews: string[];
+    userQuery: string;
+    timeRange: {
+      type: 'sliding' | 'fixed';
+      sliding?: {
+        startBacktest?: string; // during development
+        minutes: 5 | 15 | 60;
+      };
+      fixed?: {
+        start: string;
+        end: string;
+      };
+    };
+  };
+  llm: {
+    provider: string;
+    model: string;
+    temperature?: number;
+    apiKey: string;
+    framework?: string;
+
+  };
+  actions?: {
+    twitter?: {
+      enabled?: boolean;
+      apiKey: string;
+      apiSecret: string;
+      accessToken?: string;
+      accessSecret?: string;
+      username?: string;
+    }
+  };
+  knowledge: any;
+  tools: {
+    enabledTools: string[];
+    configurations: Record<string, any>;
+  };
+}
