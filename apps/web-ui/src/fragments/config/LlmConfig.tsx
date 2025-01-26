75db4c1ffe005ff1afded3163bfe1b1a11cf1564
dyslaxic <3xdevvv@gmail.com>
2025-01-26 18:52:25 +0000

    allow different providers, support local models

--- apps/web-ui/src/fragments/config/LlmConfig.tsx
+++ apps/web-ui/src/fragments/config/LlmConfig.tsx
@@ -1,18 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAgentConfig } from '../../context/useAgentContext';
+import { useApi } from '../../hooks/useApi';
+import { Button } from '../../components/ui/button';
 
 export const LLMConfig: React.FC = () => {
 
   const { config, updateConfig } = useAgentConfig();
+  const { listLocalModels, addLocalModel } = useApi()
+
+  const [providerModels, setProviderModels] = useState<any>({
+    openai: {
+      key: 'openai',
+      displayName: 'OpenAI',
+      models: [
+        'gpt-4o',
+        'gpt-4o-mini',
+        'o1-preview',
+        'o1-mini'
+      ]
+    },
+  })
+
+  const selectedProvider = config?.llm?.provider || 'openai'
+
+  useEffect(() => {
+
+    listLocalModels()
+      .then(models => {
+        setProviderModels((_models: any) => ({
+          ..._models,
+          'ollama': {
+            key: 'ollama',
+            displayName: 'Ollama',
+            models: models.map((m: any) => m.model)
+          }
+        }))
+      })
+      .catch(e => {
+        console.log('failed to get local models')
+      })
+  
+    return () => {
+    }
+  }, [])
+  
 
   const handleChange = (field: keyof typeof config.llm, value: string) => {
     updateConfig({
       type: 'UPDATE_LLM',
       payload: { [field]: value }
     });
+    if (field === 'provider') {
+      try {
+        const firstModel = providerModels[value].models[0]
+        updateConfig({
+          type: 'UPDATE_LLM',
+          payload: {
+            model: firstModel
+          }
+        })
+      } catch (error) {
+        
+      }
+    }
   };
 
-  const handleProviderChange = (_value: string) => {};
+  const handleAddDeepseek = async () => {
+    await addLocalModel()
+  }
+
+  if (!config) {
+    return <div>loading..</div>
+  }
 
   return (
     <div className="space-y-6">
@@ -20,12 +79,16 @@ export const LLMConfig: React.FC = () => {
         <h3 className="text-sm text-blue-400 mb-3">Model Settings</h3>
         <div className="space-y-4">
           <div>
+            <Button onClick={handleAddDeepseek}>Add Deepseek r1</Button>
             <label className="text-sm mb-1 block">Provider</label>
             <select 
               className="w-full bg-gray-900/50 px-4 py-2 rounded-lg border border-gray-800"
-              onChange={(e) => handleProviderChange?.(e.target.value)}
+              onChange={(e) => handleChange('provider', e.target.value)}
             >
-              <option value="openai">OpenAI</option>
+              {Object.values(providerModels).map(
+                (provider: any) => <option key={provider.key} value={provider.key}>{provider.displayName}</option>
+              )}
+              
             </select>
           </div>
           <div>
@@ -35,10 +98,9 @@ export const LLMConfig: React.FC = () => {
               onChange={(e) => handleChange('model' ,e.target.value)}
               value={config.llm.model}
             >
-              <option value="gpt-4o">gpt-4o</option>
-              <option value="gpt-4o-mini">gpt-4o-mini</option>
-              <option value="o1-preview">o1-preview</option>
-              <option value="o1-mini">o1-mini</option>
+              {providerModels[selectedProvider]?.models.map((model: any) => (
+                <option key={model} value={model}>{model}</option>
+              ))}
             </select>
           </div>
           <div>
